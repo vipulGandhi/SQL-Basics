429e6987c81a33c2f1ab58a14b4ba665adca174e
Vipul Gandhi <vipulgandhi10@gmail.com>
2023-01-30 22:22:37 +0530

    Initial commit

---
+++ src/main/java/Basics/Basics.java
@@ -0,0 +1,44 @@
+// SQL: Structured Query Language
+	// Common query language for all SQL based database
+
+// XAMPP Installation
+	// XAMPP: Cross-Platform, Apache Server, MySQL Database, PHP, and Perl
+	// Free, Open Source, Cross-Platform Web Server
+	// XAMPP gives a complete bundle package
+	// Installation
+		// Download and Install: https://www.apachefriends.org/index.html
+		// Once downloaded, Open XAMPP Manager
+		// Click Manage Server
+		// Start: Apache Web Server
+		// Start: MYSQL Database
+		// Click: Welcome, Click "Go To Application", A webpage will open
+		// Click: phpMyAdmin
+		// MySQL database server will open on the localhost: http://localhost/phpmyadmin/
+		// On the dashboard, on the left panel, observe that few databases are already created
+		// Create a new database
+			// From the left panel, click New
+				// Enter the database name (eg. SQL_Practice), click Create
+				// The database is created, Observe the left panel
+				// Click on the database
+			// Create Table in database, Add some data into it
+
+// Cross Join
+	// All rows of table 1 will be paired with all rows of table 2
+		// SELECT * FROM MatchScore CROSS JOIN Departments; 
+
+// Types Of Joins
+	// Inner Join
+		// Gives the common rows of both tables
+	// Outer Join
+		// Left Outer Join
+			// Inclusive Join
+			// Exclusive Join
+		// Right Outer Join
+			// Inclusive Join
+			// Exclusive Join
+		// Full Outer Join
+			// Inclusive Join
+			// Exclusive Join	
+
+//Ref
+	// https://www.youtube.com/playlist?list=PLFGoYjJG_fqrCj-zabjfsO2t4VpnJuCKJ
